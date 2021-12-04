1bfe6d0d66f9b32894e09615248b130e84b31a34
joshgrafil <josh_grafil@gwu.edu>
2021-12-04 13:03:56 -0500

    Audio test script, among other things

---
+++ Assets/Scripts/AudioTest.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+ using System.Collections;
+ 
+  //Add this Script Directly to The Death Zone
+ public class AudioTest : MonoBehaviour
+ {
+     public AudioClip Bell_1_b;    // Add your Audi Clip Here;
+     // This Will Configure the  AudioSource Component; 
+     // MAke Sure You added AudioSouce to death Zone;
+     void Start ()   
+     {
+         GetComponent<AudioSource> ().playOnAwake = false;
+         GetComponent<AudioSource> ().clip = Bell_1_b;
+     }        
+ 
+     void OnCollisionEnter ()  //Plays Sound Whenever collision detected
+     {
+         GetComponent<AudioSource> ().Play ();
+     }
+          // Make sure that deathzone has a collider, box, or mesh.. ect..,
+          // Make sure to turn "off" collider trigger for your deathzone Area;
+          // Make sure That anything that collides into deathzone, is rigidbody;
+ }
--- Assets/Scripts/SwipeScript.cs
+++ Assets/Scripts/SwipeScript.cs
@@ -2,6 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class SwipeScript : MonoBehaviour
+{
+    private Vector3 mousePressDownPos;
+    private Vector3 mouseReleasePos;
+
+    private Rigidbody rb;
+
+    private bool isShoot;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        mousePressDownPos = Input.mousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        mouseReleasePos = Input.mousePosition;
+        Shoot(-(mouseReleasePos-mousePressDownPos));
+    }
+
+    private float forceMultiplier = 5;
+    void Shoot(Vector3 Force)
+    {
+        if(isShoot)    
+            return;
+        
+        rb.AddForce(new Vector3(Force.x,Force.y,Force.y) * forceMultiplier);
+        isShoot = true;
+    }
+    
+}
+
+/*
 public class SwipeScript : MonoBehaviour {
 
 	Vector2 startPos, endPos, direction; // touch start position, touch end position, swipe direction
@@ -57,3 +97,4 @@ public class SwipeScript : MonoBehaviour {
 			
 	}
 }
+*/
